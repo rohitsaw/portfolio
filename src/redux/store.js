6b5864e0ceaed61018275ccd2a0b52d587f40ca7
Rohit <rsaw409@gmail.com>
2024-02-06 23:12:48 +0530

    remove redux log from production env

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducer.js";
 import logger from "redux-logger";
 
-
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    process.env.NODE_ENV !== "production"
+      ? getDefaultMiddleware().concat(logger)
+      : getDefaultMiddleware(),
   devTools: process.env.NODE_ENV !== "production",
 });
 
-export default store; 
+export default store;
