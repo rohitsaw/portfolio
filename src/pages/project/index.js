9ecf00049756c275c2dfa4a2c0b9048196dea7bf
Rohit <rsaw409@gmail.com>
2024-08-23 18:07:21 +0530

    change projects page

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -1,12 +1,33 @@
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import CircularProgress from "@mui/material/CircularProgress";
 
-import Project from "../../component/project/index.js";
+import { motion } from "framer-motion";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import styles from "./index.module.css";
 
-import { transformProjects } from "../../utils/util.js";
+import { ListItem, ListItemButton } from "@mui/material";
+
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ShopIcon from "@mui/icons-material/Shop";
+
+import {
+  faJs,
+  faNode,
+  faReact,
+  faAndroid,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { ReactComponent as FlutterLogo } from "../../icons/Google-flutter-logo.svg";
 
 const Projects = () => {
   let { isProjectsLoading, projects } = useSelector((state) => ({
@@ -14,7 +35,7 @@ const Projects = () => {
     projects: state.projects,
   }));
 
-  projects = transformProjects(projects);
+  const [hoverIndex, setHoverIndex] = useState(-1);
 
   return (
     <motion.div
@@ -41,9 +62,134 @@ const Projects = () => {
         <p>No Projects Found!</p>
       ) : (
         <div className={styles.projectsList}>
-          {projects.map((project) => (
-            <Project key={project.id} project={project} />
-          ))}
+          <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+            <List component="nav">
+              {projects.map((each, index) => (
+                <>
+                  <ListItemButton
+                    onMouseEnter={() => setHoverIndex(index)}
+                    onMouseLeave={() => setHoverIndex(-1)}
+                  >
+                    <ListItem alignItems="flex-start">
+                      <NavLink
+                        to={each.github_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <ListItemIcon>
+                          <GitHubIcon
+                            sx={{ p: 0 }}
+                            style={{
+                              color:
+                                hoverIndex === index
+                                  ? `var(--primary-color)`
+                                  : undefined,
+                            }}
+                          />
+                          ,
+                        </ListItemIcon>
+                      </NavLink>
+
+                      <ListItemText
+                        primary={each.project_name}
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {each.project_description}
+                              <br />
+                              <br />
+
+                              <Box display="flex" style={{ gap: 20 }}>
+                                {each.technology_tags.includes("flutter") && (
+                                  <FlutterLogo height={22} />
+                                )}
+
+                                {each.technology_tags.includes("react") && (
+                                  <FontAwesomeIcon
+                                    icon={faReact}
+                                    style={{ color: "#5ED2F3" }}
+                                    size="xl"
+                                  />
+                                )}
+
+                                {each.technology_tags.includes("node") && (
+                                  <FontAwesomeIcon
+                                    icon={faNode}
+                                    style={{ color: "#57A745" }}
+                                    size="2xl"
+                                  />
+                                )}
+
+                                {each.technology_tags.includes(
+                                  "javascript"
+                                ) && (
+                                  <FontAwesomeIcon
+                                    icon={faJs}
+                                    style={{ color: "#57A745" }}
+                                    size="xl"
+                                  />
+                                )}
+
+                                {each.technology_tags.includes("android") && (
+                                  <FontAwesomeIcon
+                                    icon={faAndroid}
+                                    style={{ color: "#57A745" }}
+                                    size="xl"
+                                  />
+                                )}
+                              </Box>
+                            </Typography>
+                          </React.Fragment>
+                        }
+                      />
+
+                      {each.play_store_url && (
+                        <NavLink
+                          to={each.play_store_url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <ShopIcon
+                            sx={{ pl: 1 }}
+                            style={{
+                              color:
+                                hoverIndex === index
+                                  ? `var(--primary-color)`
+                                  : undefined,
+                            }}
+                          />
+                        </NavLink>
+                      )}
+
+                      {each.web_url && (
+                        <NavLink
+                          to={each.web_url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <ArrowOutwardIcon
+                            sx={{ pl: 1 }}
+                            style={{
+                              color:
+                                hoverIndex === index
+                                  ? `var(--primary-color)`
+                                  : undefined,
+                            }}
+                          />
+                        </NavLink>
+                      )}
+                    </ListItem>
+                  </ListItemButton>
+                  <Divider variant="middle" component="li" />
+                </>
+              ))}
+            </List>
+          </Box>
         </div>
       )}
     </motion.div>
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,16 +1,3 @@
-import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
-import ShopIcon from "@mui/icons-material/Shop";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import {
-  faJs,
-  faNode,
-  faReact,
-  faAndroid,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { ReactComponent as FlutterLogo } from "../icons/Google-flutter-logo.svg";
-
 function groupBy(array, callbackFn) {
   let obj = {};
   for (let each of array) {
@@ -37,70 +24,4 @@ function transformSkills(skills) {
   }
 }
 
-const transformProjects = (projects) => {
-  return projects?.map((each) => {
-    const links = [];
-    if (each.github_url && each.github_url !== "NULL") {
-      links.push({
-        getIcon: () => <GitHubIcon />,
-        url: each.github_url,
-      });
-    }
-    if (each.play_store_url && each.play_store_url != "NULL") {
-      links.push({
-        getIcon: () => <ShopIcon />,
-        url: each.play_store_url,
-      });
-    }
-    if (each.web_url && each.web_url !== "NULL") {
-      links.push({
-        getIcon: () => <ArrowOutwardIcon />,
-        url: each.web_url,
-      });
-    }
-
-    return {
-      id: each.id,
-      title: each.project_name,
-      description: each.project_description,
-      links: links,
-      getLogo: () => (
-        <>
-          {each.technology_tags?.includes("react") && (
-            <FontAwesomeIcon
-              icon={faReact}
-              style={{ color: "#5ED2F3" }}
-              size="xl"
-            />
-          )}
-          {each.technology_tags?.includes("android") && (
-            <FontAwesomeIcon
-              icon={faAndroid}
-              style={{ color: "#9FC036" }}
-              size="xl"
-            />
-          )}
-          {each.technology_tags?.includes("node") && (
-            <FontAwesomeIcon
-              icon={faNode}
-              style={{ color: "#57A745" }}
-              size="2xl"
-            />
-          )}
-          {each.technology_tags?.includes("javascript") && (
-            <FontAwesomeIcon
-              icon={faJs}
-              style={{ color: "#EFD81A" }}
-              size="xl"
-            />
-          )}
-          {each.technology_tags?.includes("flutter") && (
-            <FlutterLogo height={22} />
-          )}
-        </>
-      ),
-    };
-  });
-};
-
-export { groupBy, transformSkills, transformProjects };
+export { groupBy, transformSkills };
