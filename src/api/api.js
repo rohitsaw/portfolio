90dc3bbe37a30851ec666bc19ea272022cb0ea5f
Rohit <rsaw409@gmail.com>
2024-10-25 12:55:58 +0530

    change base api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,6 @@
 let base_url =
   process.env.NODE_ENV === "production"
-    ? "https://backend.portfolio.rsaw409.me"
-    : "http://localhost:3000";
-
-// axios.defaults.withXSRFToken = true
+    ? "https://backend.portfolio.rsaw409.me/portfolio"
+    : "http://localhost:3000/portfolio";
 
 export { base_url };
